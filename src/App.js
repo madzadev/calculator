adedf96e09b178b44d7300e46b940524159d6fc5
madarsbiss <madars.biss@gmail.com>
2021-08-25 16:06:56 +0300

    add refactor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Textfit } from "react-textfit";
 import "./App.css";
 
+import validateNum from "./validation/validate";
+
 const Screen = ({ res }) => {
   return (
     <Textfit className="screen-wrapper" mode="single" max={70}>
@@ -26,24 +28,6 @@ const btnValues = [
   [0, ".", "="],
 ];
 
-// const toLocaleString = (x, sep, grp) => {
-//   var sx = ("" + x).split("."),
-//     s = "",
-//     i,
-//     j;
-//   sep || (sep = " ");
-//   grp || grp === 0 || (grp = 3);
-//   i = sx[0].length;
-//   while (i > grp) {
-//     j = i - grp;
-//     s = sep + sx[0].slice(j, i) + s;
-//     i = j;
-//   }
-//   s = sx[0].slice(0, i) + s;
-//   sx[0] = s;
-//   return sx.join(".");
-// };
-
 // https://stackoverflow.com/questions/3753483/javascript-thousand-separator-string-format
 
 const toLocaleString = (num) =>
@@ -58,35 +42,54 @@ const App = () => {
     res: 0,
   });
 
+  // const numClick = (e) => {
+  //   const value = e.target.innerHTML;
+  //   if (
+  //     (calc.num === "0" && value === "0") || //avoid entering 00034
+  //     (calc.num.toString().includes(".") && value === ".") || //avoid entering 0..34
+  //     (calc.res && !calc.num && value === ".") //avoid adding comma in the result
+  //   )
+  //     return;
+
+  //   if (!calc.sign) {
+  //     calc.res = 0; //reset after equal press
+  //   }
+
+  //   if (removeSpaces(calc.num).length < 16) {
+  //     setCalc({
+  //       ...calc,
+  //       num:
+  //         !calc.num && value === "." // format to 0. if . pressed first
+  //           ? "0."
+  //           : calc.num && value === "." // add comma for number
+  //             ? calc.num + "."
+  //             : toLocaleString(
+  //               (!calc.num || calc.num === "0" ? value : (calc.num += value))
+  //                 .toString()
+  //                 .replace(/\s/g, "")
+  //             ),
+  //     });
+  //   }
+  // };
+
   const numClick = (e) => {
     const value = e.target.innerHTML;
-    if (
-      (calc.num === "0" && value === "0") || //avoid entering 00034
-      (calc.num.toString().includes(".") && value === ".") || //avoid entering 0..34
-      (calc.res && !calc.num && value === ".") //avoid adding comma in the result
-    )
-      return;
+    setCalc({
+      ...calc,
+      num: calc.num % 1 === 0 ? Number(calc.num + value) : calc.num + value,
+      res: !calc.sign ? 0 : calc.res //if no sign set, start a new calc
+    });
 
-    if (!calc.sign) {
-      calc.res = 0; //reset after equal press
-    }
-
-    if (removeSpaces(calc.num).length < 16) {
-      setCalc({
-        ...calc,
-        num:
-          !calc.num && value === "." // format to 0. if . pressed first
-            ? "0."
-            : calc.num && value === "." // add comma for number
-            ? calc.num + "."
-            : toLocaleString(
-                (!calc.num || calc.num === "0" ? value : (calc.num += value))
-                  .toString()
-                  .replace(/\s/g, "")
-              ),
-      });
-    }
+    // console.log(calc.num, calc.res)
   };
+
+  const comaClick = (e) => {
+    const value = e.target.innerHTML;
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? (calc.num + value) : calc.num
+    });
+  }
 
   const invert = () => {
     setCalc({
@@ -117,56 +120,89 @@ const App = () => {
     });
   };
 
+  // const result = () => {
+  //   const [conNum, conRes] = [
+  //     Number(calc.sign && calc.num ? removeSpaces(calc.num) : 0),
+  //     Number(calc.res ? removeSpaces(calc.res) : 0),
+  //   ];
+
+  //   const math = (a, b, sign) =>
+  //     sign === "+"
+  //       ? a + b
+  //       : sign === "-"
+  //         ? a - b
+  //         : sign === "X"
+  //           ? a * b
+  //           : a / b;
+
+  //   const total = math(conRes, conNum, calc.sign);
+  //   if (calc.sign && calc.num) {
+  //     //to prevent repetitive equals press
+  //     setCalc({
+  //       ...calc,
+  //       res:
+  //         calc.num === "0" && calc.sign === "/"
+  //           ? "Can't divide with 0"
+  //           : toLocaleString(total),
+  //       num:
+  //         !calc.res ||
+  //           (!calc.res && calc.sign === "X") ||
+  //           (!calc.res && calc.sign === "/")
+  //           ? calc.num
+  //           : 0,
+  //       sign: "",
+  //     });
+  //   }
+
+  //   if (calc.num && calc.sign.length === 1) return total;
+  // };
+
   const result = () => {
-    const [conNum, conRes] = [
-      Number(calc.sign && calc.num ? removeSpaces(calc.num) : 0),
-      Number(calc.res ? removeSpaces(calc.res) : 0),
-    ];
+    if (calc.sign && calc.num) { //to prevent multiple equal presses
 
-    const math = (a, b, sign) =>
-      sign === "+"
-        ? a + b
-        : sign === "-"
-        ? a - b
-        : sign === "X"
-        ? a * b
-        : a / b;
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : sign === "-"
+            ? a - b
+            : sign === "X"
+              ? a * b
+              : a / b;
 
-    const total = math(conRes, conNum, calc.sign);
-    if (calc.sign && calc.num) {
-      //to prevent repetitive equals press
       setCalc({
         ...calc,
-        res:
-          calc.num === "0" && calc.sign === "/"
-            ? "Can't divide with 0"
-            : toLocaleString(total),
-        num:
-          !calc.res ||
-          (!calc.res && calc.sign === "X") ||
-          (!calc.res && calc.sign === "/")
-            ? calc.num
-            : 0,
-        sign: "",
-      });
+        res: calc.num === "0" && calc.sign === "/" //case to divide with 0
+          ? "Can't divide with 0"
+          : math(Number(calc.res), Number(calc.num), calc.sign),
+        sign: '',
+        num: 0
+      })
     }
+  }
 
-    if (calc.num && calc.sign.length === 1) return total;
-  };
+  // const arithmetics = (e) => {
+  //   setCalc({
+  //     ...calc,
+  //     sign: e.target.innerHTML,
+  //     res:
+  //       !calc.res && calc.num
+  //         ? toLocaleString(Number(removeSpaces(calc.num)))
+  //         : calc.res && calc.num && calc.sign
+  //           ? toLocaleString(result()) //2+2+2 without equal
+  //           : calc.res, //for repetitive arithmetic presses
+  //     num: 0,
+  //   });
+  // };
 
   const arithmetics = (e) => {
     setCalc({
       ...calc,
       sign: e.target.innerHTML,
-      res:
-        !calc.res && calc.num
-          ? toLocaleString(Number(removeSpaces(calc.num)))
-          : calc.res && calc.num && calc.sign
-          ? toLocaleString(result()) //2+2+2 without equal
-          : calc.res, //for repetitive arithmetic presses
+      res: !calc.res && calc.num ? calc.num : calc.res, //for repeated sign presses
       num: 0,
     });
-  };
+
+  }
 
   return (
     <div className="calc-wrapper">
@@ -182,14 +218,14 @@ const App = () => {
                 btn === "C"
                   ? reset
                   : btn === "+-"
-                  ? invert
-                  : btn === "%"
-                  ? percent
-                  : btn === "="
-                  ? result
-                  : btn === "/" || btn === "X" || btn === "-" || btn === "+"
-                  ? arithmetics
-                  : numClick
+                    ? invert
+                    : btn === "%"
+                      ? percent
+                      : btn === "="
+                        ? result
+                        : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                          ? arithmetics : btn === "." ? comaClick
+                            : numClick
               }
             />
           );
